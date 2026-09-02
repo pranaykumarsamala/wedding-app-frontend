f1e498cb5c382d5d90c25e8e567c3a8bf5e4ceab
DESKTOP-PLV51FF\DELL <kasarlabhargavi03@gmail.com>
2026-09-02 12:42:41 +0530

    gallery, text scss

--- src/app/templateone/gallery/gallery.ts
+++ src/app/templateone/gallery/gallery.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-gallery',
   imports: [],
   templateUrl: './gallery.html',
   styleUrl: './gallery.scss',
 })
-export class Gallery {
-
-}
+export class Gallery {}
